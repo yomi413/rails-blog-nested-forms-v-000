0798f16e4b19b25e39e9324f8a7d0c3793b989b5
Yomaira Escano <yjescano@gmail.com>
2018-04-09 21:19:12 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
+  accepts_nested_attributes_for :tags, reject_if: :if_blank
 
   validates_presence_of :name, :content
+
+  def if_blank(attributes)
+    attributes[:name].blank?
+  end
 end
