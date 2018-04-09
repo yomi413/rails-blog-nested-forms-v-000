1175632173862d0dfd33050a543c9024987cd9b0
Yomaira Escano <yjescano@gmail.com>
2018-04-09 20:52:21 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,11 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
-  accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes[:name].blank?}
+  accepts_nested_attributes_for :tags, reject_if: :reject_tags
 
   validates_presence_of :name, :content
 
-  
+
 
   def reject_tags(attributes)
     attributes[:name].blank?
